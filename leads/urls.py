117124a551da121a802b980eeedb62eebb12c91e
Dan Deng <hui_danny_deng@hotmail.com>
2021-07-31 17:30:28 -0400

    add CRUD featrue for Lead

--- leads/urls.py
+++ leads/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import lead_list, lead_details
+from .views import lead_list, lead_details, lead_create, lead_update, lead_delete
 
 app_name = 'leads'
 
 urlpatterns = [
-    path('', lead_list),
-    path('<pkey>', lead_details),
+    path('', lead_list, name='lead-list'),
+    path('<int:pkey>/', lead_details, name='lead-details'),
+    path('create/', lead_create, name='lead-create'),
+    path('<int:pkey>/update/', lead_update, name='lead-update'),
+    path('<int:pkey>/delete/', lead_delete, name='lead-delete'),
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect   
 from django.http import HttpResponse
-from .models import Lead
+from .models import Lead, SalesPerson
+from .forms import LeadForm, LeadModelForm
 
 # Create your views here.
 
@@ -20,3 +21,56 @@ def lead_details(request, pkey):
         "lead":lead,
     }
     return render(request, "leads/lead_details.html", context=context)
+
+def lead_create(request):
+    print(request.POST)
+    form=LeadModelForm()
+    if(request.method == "POST"):
+        form=LeadModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/leads')
+    context ={
+        "form": form
+    }
+    return render(request, "leads/lead_create.html", context=context)
+
+def lead_update(request, pkey):
+    lead=Lead.objects.get(id=pkey)
+    form=LeadModelForm(instance=lead)
+    if(request.method == "POST"):
+        form=LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect('/leads')
+    context ={
+        "form": form,
+        "lead":lead
+    }
+    return render(request, "leads/lead_update.html", context=context)
+
+def lead_delete(request, pkey):
+    lead=Lead.objects.get(id=pkey)
+    lead.delete()
+    return redirect('/leads')
+
+def lead_create_customform(request):
+    print(request.POST)
+    form=LeadForm()
+    if(request.method == "POST"):
+        form=LeadForm(request.POST)
+        if form.is_valid():
+            first_name=form.cleaned_data['first_name']
+            last_name=form.cleaned_data['last_name']
+            age=form.cleaned_data['age']
+            agent=SalesPerson.objects.first()
+            Lead.objects.create(    
+                    first_name=first_name, 
+                    last_name=last_name, 
+                    age=age, 
+                    agent=agent)
+            return redirect('/leads')
+    context ={
+        "form": form
+    }
+    return render(request, "leads/lead_create.html", context=context)
