f2006235c684c84bcffcabce61f4c212a516324c
Dan Deng <hui_danny_deng@hotmail.com>
2021-07-30 21:19:51 -0400

    add file to githoub

---
+++ leads/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class SalesAgent(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
+
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    profile_image = models.ImageField(blank=True, null=True)
+    special_file = models.FileField(blank=True, null=True)
+    phoned= models.BooleanField(default=False)
+    agent = models.ForeignKey("SalesAgent", on_delete=models.CASCADE)
